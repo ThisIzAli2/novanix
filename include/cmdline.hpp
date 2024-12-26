d11307f074bf33177be47c85ccc41eed36dee3e6
Heisenberg <alimirmohammad.1386@gmail.com>
2024-12-26 14:16:02 -0500

    `Update cmdline.hpp: change printk color argument from 1 to 0`

--- include/cmdline.hpp
+++ include/cmdline.hpp
@@ -8,7 +8,8 @@ inline void cmdline(){
     do{
         read_key();
         // printk(VGA_COLOR_WHITE,stringify(read_key()),1); // 4 test
-        Novanix::system::printk(Novanix::system::VGA_COLOR_WHITE,">/",1);
+        Novanix::system::printk(Novanix::system::VGA_COLOR_WHITE,">/",0);
         Novanix::system::printk(Novanix::system::VGA_COLOR_WHITE,handle_keyboard(read_key()),0);
+
     }while(read_key() != 0);
 }
