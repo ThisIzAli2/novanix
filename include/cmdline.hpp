2cb1861cd7773cc14a3704b16af20b3b320f413c
Heisenberg <alimirmohammad.1386@gmail.com>
2024-12-26 14:54:31 -0500

    `Fix typo in cmdline function: '>' instead of '>/'`

--- include/cmdline.hpp
+++ include/cmdline.hpp
@@ -6,10 +6,10 @@
 
 inline void cmdline(){
     const char* cmd;
-    Novanix::system::printk(Novanix::system::VGA_COLOR_WHITE,">/",0);
+    Novanix::system::printk(Novanix::system::VGA_COLOR_WHITE,">",0);
     do{
         if (cmd == "\n"){
-        Novanix::system::printk(Novanix::system::VGA_COLOR_WHITE,">/",0);
+        Novanix::system::printk(Novanix::system::VGA_COLOR_WHITE,">",0);
 
         }
 
@@ -17,6 +17,5 @@ inline void cmdline(){
         // printk(Novanix::system::VGA_COLOR_WHITE,stringify(read_key()),1); // 4 test
         cmd = handle_keyboard(read_key());
         Novanix::system::printk(Novanix::system::VGA_COLOR_WHITE,cmd,0);
-
     }while(read_key() != 0);
 }
