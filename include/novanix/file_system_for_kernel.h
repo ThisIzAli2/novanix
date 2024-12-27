c3b077d8234b2edeae66c8d8dc11ad791e54279a
Heisenberg <alimirmohammad.1386@gmail.com>
2024-12-27 18:13:01 -0500

    `Added FileSystem class with directory structure and findDirectory function`

--- include/novanix/file_system_for_kernel.h
+++ include/novanix/file_system_for_kernel.h
@@ -14,6 +14,26 @@ using namespace Novanix::system;
 
 
 
-class
+class FileSystem{
+    private:
+     char currentDirectory[CURRENT_DIR_BUFFER_SIZE];
+    struct Directory {
+        const char* name;
+        Directory* subdirectories;
+        int subdirectoryCount;
+    } root;
+
+    Directory* findDirectory(Directory* dir,const char* name){
+        for (int i = 0;i < dir->subdirectoryCount;i++){
+            if (Novanix::common::String::strcmp(dir->subdirectories[i].name, name)){
+                    return &dir->subdirectories[i];
+
+            }
+        }
+        return nullptr;
+    } 
+    
+    
+};
 
 #endif
