03689c7626534504b39a6a89463dbfda1b7dbcaa
Heisenberg <alimirmohammad.1386@gmail.com>
2024-12-26 21:14:27 -0500

    `Added typing.hpp include and modified inline functions to use __always_inline attribute`

--- include/shutdownsys.h
+++ include/shutdownsys.h
@@ -2,12 +2,13 @@
 #define __NOVANIX_KERNEL_SHUT_DOWN_SYS_H
 
 #include <common/types.h>
+#include <typing.hpp>
 
-static inline void outw(Novanix::common::uint16_t port, Novanix::common::uint16_t value) {
+static __always_inline VOID outw(Novanix::common::uint16_t port, Novanix::common::uint16_t value) {
     __asm__ volatile ("outw %0, %1" : : "a"(value), "Nd"(port));
 }
 
-void inline shutdown_sys_now(){
+VOID __always_inline shutdown_sys_now(){
     outw(0x604, 0x2000); // Port 0x604 is common for ACPI shutdown
 }
 
