6296fa85e8510e19b376d7fbf1c1ccaf7924fe2e
Heisenberg <alimirmohammad.1386@gmail.com>
2024-12-29 19:52:36 -0500

    `Update console.h: Add/modify function declarations and include typing.hpp`

--- include/console.h
+++ include/console.h
@@ -2,27 +2,28 @@
 #define CONSOLE_H
 
 #include <fs/vfs.h>
+#include <typing.hpp>
 
-void console_init(char *usr);
-void console_init_gui(char *usr);
-void console_run();
-void console_run_gui();
-void console_gui_print(char *str);
+VOID console_init(char *usr);
+VOID console_init_gui(char *usr);
+VOID console_run();
+VOID console_run_gui();
+VOID console_gui_print(char *str);
 
 char *get_dir();
-int character_check(char c);
-void console_print(char *buffer, ...);
-void console_exec(char *buf);
-void console_cd(char *dir, char *command);
-void console_start(char *dir, char *command);
-void console_read(char *dir, char *command);
-void console_write(char *dir, char *command);
-void console_touch(char *dir, char *command);
-void console_delete(char *dir, char *command);
+INTEGER character_check(char c);
+VOID console_print(char *buffer, ...);
+VOID console_exec(char *buf);
+VOID console_cd(char *dir, char *command);
+VOID console_start(char *dir, char *command);
+VOID console_read(char *dir, char *command);
+VOID console_write(char *dir, char *command);
+VOID console_touch(char *dir, char *command);
+VOID console_delete(char *dir, char *command);
 char *console_pwd();
 char *console_pwd_user();
-void print_file(file *f);
-void print_meminfo();
-char *get_argument(char *command, int n);
+VOID print_file(file *f);
+VOID print_meminfo();
+char *get_argument(char *command, INTEGER n);
 
 #endif
