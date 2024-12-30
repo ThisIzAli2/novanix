84629503bc59c0d7b33d3ecb49e85b09de9f02e4
Heisenberg <alimirmohammad.1386@gmail.com>
2024-12-29 20:03:50 -0500

    `vga_switcheroo.h: Add typing.hpp include and modify function types`
    This commit message accurately describes the changes made in the provided code diff.

--- include/alinix/vga_switcheroo.h
+++ include/alinix/vga_switcheroo.h
@@ -24,6 +24,8 @@
 #ifndef __ALINIX_KERNEL_VGA_SWITCH_HEROO_H
 #define __ALINIX_KERNEL_VGA_SWITCH_HEROO_H
 
+#include <typing.hpp>
+
 enum vga_switcheroo_client_id {
 	VGA_SWITCHEROO_UNKNOWN_ID = 0x1000,
 	VGA_SWITCHEROO_IGD = 0,
@@ -32,10 +34,10 @@ enum vga_switcheroo_client_id {
 };
 
 struct vga_switcheroo_handler {
-	int (*init)(void);
-	int (*switchto)(enum vga_switcheroo_client_id id);
-	int (*switch_ddc)(enum vga_switcheroo_client_id id);
-	int (*power_state)(enum vga_switcheroo_client_id id,
+	INTEGER (*init)(VOID);
+	INTEGER (*switchto)(enum vga_switcheroo_client_id id);
+	INTEGER (*switch_ddc)(enum vga_switcheroo_client_id id);
+	INTEGER (*power_state)(enum vga_switcheroo_client_id id,
 			   enum vga_switcheroo_state state);
 	enum vga_switcheroo_client_id (*get_client_id)(struct pci_dev *pdev);
 };
@@ -43,10 +45,10 @@ struct vga_switcheroo_handler {
 
 
 struct vga_switcheroo_client_ops {
-	void (*set_gpu_state)(struct pci_dev *dev, enum vga_switcheroo_state);
-	void (*reprobe)(struct pci_dev *dev);
+	VOID (*set_gpu_state)(struct pci_dev *dev, enum vga_switcheroo_state);
+	VOID (*reprobe)(struct pci_dev *dev);
 	bool (*can_switch)(struct pci_dev *dev);
-	void (*gpu_bound)(struct pci_dev *dev, enum vga_switcheroo_client_id);
+	VOID (*gpu_bound)(struct pci_dev *dev, enum vga_switcheroo_client_id);
 };
 
 
