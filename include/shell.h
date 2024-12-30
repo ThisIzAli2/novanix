31474ab293c21367943cf83bda128aa75ab41f5a
Heisenberg <alimirmohammad.1386@gmail.com>
2024-12-29 19:57:04 -0500

    `Update include/shell.h: Add typing.hpp, change callback type to VOID, and update function prototypes to match VOID return type.`

--- include/shell.h
+++ include/shell.h
@@ -1,23 +1,25 @@
 #ifndef _HEISEN_SHELL_H
 #define _HEISEN_SHELL_H
 
+#include <typing.hpp>
+
 #include <typing.h>
 
 typedef struct command{
     uint8_t present;
     char* command;
-    void* callback;
+    VOID* callback;
     char* description;
 }command_t;
 
-void shellHandleKey(uint32_t keycode,char ch);
-void shellInit();
-void shellPrintKernel();
-void shellHandleCommand();
-void shellResetBuffer();
-void registerCommand(char* command,void* callback,char * description);
-void helpMenu();
-void pung();
+VOID shellHandleKey(uint32_t keycode,char ch);
+VOID shellInit();
+VOID shellPrintKernel();
+VOID shellHandleCommand();
+VOID shellResetBuffer();
+VOID registerCommand(char* command,VOID* callback,char * description);
+VOID helpMenu();
+VOID pung();
 
 
 #endif // _HEISEN_SHELL_H
