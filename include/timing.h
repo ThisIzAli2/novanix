2c6124428345d597f148db2500fe61eb92cef2e1
Heisenberg <alimirmohammad.1386@gmail.com>
2025-06-23 18:00:18 -0400

    New updates

---
+++ include/timing.h
@@ -0,0 +1,15 @@
+#ifndef __TIMING_H_NOVANIX
+#define __TIMING_H_NOVANIX
+
+
+
+#include <common/types.h>
+#include <statements.h>
+#include <common/init.hpp>
+
+
+DECLARE_NAME_SPACE
+
+
+
+#endif // __TIMING_H_NOVANIX
