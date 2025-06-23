284584a9db2cb48c17e4c4799ecb4346f9796cc6
Heisenberg <alimirmohammad.1386@gmail.com>
2025-06-23 17:34:46 -0400

    `Update help menu definition in help.hpp`

--- include/help.hpp
+++ include/help.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
 
-#define HELP_MENU " \n -h, --help         Show this help message and exit\n" 
+#define HELP_MENU " \n -h, --help         Show this help message and exit\n shutdown-now Sht down the system \n" 
