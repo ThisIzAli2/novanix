00267ef8144cd6c6fbd9696a2711bb821ed1c355
Heisenberg <alimirmohammad.1386@gmail.com>
2025-06-26 14:17:42 -0400

    Updates

--- include/cmdline.hpp
+++ include/cmdline.hpp
@@ -244,6 +244,9 @@ __always_inline VOID cmdline() {
                     ELIF(cmd_cmp(full_cmd,"heart") == 0){
                         Novanix::system::printk(VGA_WHITE,"\x03\n",1);
                     }
+                    ELIF (cmd_cmp(full_cmd,"chess") == 0){
+                        Novanix::system::printk(VGA_WHITE,"\xE2\x99\x94\n",1);
+                    }
 
                     ELIF(full_cmd[0] == 's' && full_cmd[1] == 'e' && full_cmd[2] == 't' && full_cmd[3] == '-' && full_cmd[4] == 'p' && full_cmd[5] == 'a' && full_cmd[6] == 's' && full_cmd[7] == 's' && full_cmd[8] == 'w' && full_cmd[9] == 'o' && full_cmd[10] == 'r' && full_cmd[11] == 'd'){
                         INTEGER *i_pass = new INTEGER;
