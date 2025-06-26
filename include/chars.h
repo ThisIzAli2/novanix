2b9f0a40de95a1e84a2970d8c9d7649c11dbafc3
Heisenberg <alimirmohammad.1386@gmail.com>
2025-06-26 14:20:11 -0400

    SPECIAL CHARACTERS ADDED

---
+++ include/chars.h
@@ -0,0 +1,12 @@
+#ifndef __NOVANIX_CHARS_H
+#define __NOVANIX_CHARS_H
+
+#define WHITE_SMILE_FACE 	"\x01\n"
+#define BLACK_SMILE_FACE    "\x02\n"
+#define HEART   "\x03\n"
+#define DIAMOND     	"\x04\n"
+#define CLUB    	"\x05\n"
+
+
+
+#endif // __NOVANIX_CHARS_H
