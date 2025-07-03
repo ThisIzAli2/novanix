eb7284be37fee17f4d2c66e0398841e9344b6934
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-02 22:59:12 -0400

    update cmdline.hpp

--- include/cmdline.hpp
+++ include/cmdline.hpp
@@ -160,6 +160,7 @@ __always_inline VOID cmdline() {
                         tcp_receive(19216811,19216811,(uint8_t*)3,19);
                     }
                     ELIF (cmd_cmp(full_cmd,"net-udp") == 0){
+                        //! Not ready to use yet
                         Novanix::system::printk(Novanix::system::VGA_COLOR_RED,"UDP function not ready yet",1);
                         udp_receive(19216811,19216811,(uint8_t*)3,19);
                     }
