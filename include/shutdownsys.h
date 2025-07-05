df7b28a9b0e064816c7f7e462c8c4f322e513e19
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-05 14:58:04 -0400

    update shutdownsys.h

--- include/shutdownsys.h
+++ include/shutdownsys.h
@@ -1,4 +1,22 @@
-#ifndef __NOVANIX_KERNEL_SHUT_DOWN_SYS_H
+
+/**
+ * @author Ali Mirmohammad
+ * @file shutdownsys.h
+ ** This file is part of Novanix.
+
+**Novanix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**Novanix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with Novanix. If not, see <https://www.gnu.org/licenses/>.
+*/#ifndef __NOVANIX_KERNEL_SHUT_DOWN_SYS_H
 #define __NOVANIX_KERNEL_SHUT_DOWN_SYS_H
 
 #include <common/types.h>
