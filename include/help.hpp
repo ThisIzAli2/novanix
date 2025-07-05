17e8083ee1e1f7328ee4d683e10fdc959259b648
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-05 14:54:01 -0400

    update help.hpp

--- include/help.hpp
+++ include/help.hpp
@@ -20,4 +20,4 @@
 #pragma once
 
 
-#define HELP_MENU " \n -h, --help         Show this help message and exit\n shutdown-now Sht down the system \n initfile [filename] Make a new file\n time: get the current time of the system\n" 
+#define HELP_MENU " \n -h, --help         Show this help message and exit\n shutdown-now Sht down the system \n initfile [filename] Make a new file\n time: get the current time of the system\n clear : clean the terminal" 
