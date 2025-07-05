b6644c0b9f3ba79bd85771a0872ceca8f939ac89
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-05 17:28:18 -0400

    feat: create file.h

---
+++ include/file.h
@@ -0,0 +1,22 @@
+#ifndef __NOVA_KERNEL_FILE_H
+#define __NOVA_KERNEL_FILE_H
+
+#define MAX_FILES 8
+#define MAX_FILE_SIZE 4096
+#define MAX_FILENAME_LEN 64
+
+
+
+typedef struct {
+    char name[MAX_FILENAME_LEN];
+    char content[MAX_FILE_SIZE];
+    int size;
+    int cursor;
+    int used;
+};
+
+
+
+
+
+#endif // __NOVA_KERNEL_FILE_H
