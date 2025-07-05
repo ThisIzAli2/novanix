dc9bb577db83a093bac76fe6dfe1954cc04e217d
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-05 17:21:34 -0400

    update clock.cpp

--- libcx/apps/clock/clock.cpp
+++ libcx/apps/clock/clock.cpp
@@ -1,3 +1,21 @@
+/**
+ * @author Ali Mirmohammad
+ * @file clock.cpp
+ ** This file is part of Novanix.
+
+**Novanix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**Novanix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with Novanix. If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <new.h>
 #include <gui/gui.h>
 #include <gui/directgui.h>
