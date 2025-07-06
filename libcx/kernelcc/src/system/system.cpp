3fb374db74831da1d94da66c72164e55db4eeb13
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-06 00:11:51 -0400

    update system.cpp

--- libcx/kernelcc/src/system/system.cpp
+++ libcx/kernelcc/src/system/system.cpp
@@ -1,5 +1,6 @@
 /**
  ** This file is part of Novanix.
+ @file system.cpp
 
 **AliNix is free software: you can redistribute it and/or modify
 **it under the terms of the GNU Affero General Public License as published by
