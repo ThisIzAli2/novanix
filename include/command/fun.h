109f898de60e30f322cb4ecea376a48a175e0e61
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-05 21:54:46 -0400

    feat: create fun.h

---
+++ include/command/fun.h
@@ -0,0 +1,9 @@
+#ifndef __NOVANIX_KERNEL_FUN_H
+#define __NOVANIX_KERNEL_FUN_H
+#define COW_STRING \
+"  \\   ^__^\n" \
+"       \\  (oo)\\_______\n" \
+"          (__)\\       )\\/\\\n" \
+"              ||----w |\n" \
+"              ||     ||\n"
+#endif /* __NOVANIX_KERNEL_FUN_H*/
