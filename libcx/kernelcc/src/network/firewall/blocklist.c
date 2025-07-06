ce2956310140753424df9fb4b1fbb1802b5dc692
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-05 21:21:48 -0400

    update blocklist.c

--- libcx/kernelcc/src/network/firewall/blocklist.c
+++ libcx/kernelcc/src/network/firewall/blocklist.c
@@ -2,3 +2,13 @@
 
 
 
+Node* allocate_node(){
+    for (int i = 0; i < MAX_FIREWALL_IPS;i++){
+        if (node_used[i] == 0){
+            node_used[i] = 1;
+            node_pool[i].next = 0;
+            return &node_pool[i];
+        }
+    }
+    return 0;
+}
