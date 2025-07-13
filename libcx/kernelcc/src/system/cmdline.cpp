f7142f87f2caedb96d8d865efe992a1779fafd26
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-13 01:32:43 -0400

    update cmdline.cpp

--- libcx/kernelcc/src/system/cmdline.cpp
+++ libcx/kernelcc/src/system/cmdline.cpp
@@ -398,7 +398,7 @@ VOID cmdline() {
                     i++;
                 }
             } else {
-                Novanix::system::printk(Novanix::system::VGA_COLOR_RED,"\nThe entered command is too long, cannot be handled by the buffer.\n",0);
+                Novanix::system::printk(Novanix::system::VGA_COLOR_RED,"\nThe entered prompt is too long.\n",0);
                 Novanix::common::MemoryOperations::memset(full_cmd,0,sizeof(full_cmd));
                 Novanix::system::printk(Novanix::system::VGA_COLOR_RED,"\n",0);
                 PRINT_PROMPT
