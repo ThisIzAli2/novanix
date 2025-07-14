828b67bb02cd6b7e6618c81ffd6bfeacfe9d7724
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-13 20:08:45 -0400

    feat: create fcreate.h and fcreate.cpp

---
+++ include/software/fcreate.h
@@ -0,0 +1,28 @@
+/**
+ * @author Ali Mirmohammad
+ * @file fcreate.h
+ ** This file is part of Novanix.
+
+**Novanix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**Novanix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with Novanix. If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef __FCREATE_NOVANIX_H
+#define __FCREATE_NOVANIX_H
+
+
+#include <common/init.hpp>
+
+
+void create_file(const char* name,char* data,int size);
+
+#endif /*__FCREATE_NOVANIX_H*/
