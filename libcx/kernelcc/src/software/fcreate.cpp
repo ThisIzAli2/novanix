68b692bb843021edd53facc7c31ae67c4996dee6
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-14 17:41:09 -0400

    update fcreate.cpp

--- libcx/kernelcc/src/software/fcreate.cpp
+++ libcx/kernelcc/src/software/fcreate.cpp
@@ -29,7 +29,9 @@ using namespace Novanix::system;
 static uint32_t seed = 1;
 
 
-
+/**
+ * @brief A function that creates a file for the system
+ */
 file_t __create_file(char* name, char* data, int size) {
     file_t file;
     static INTEGER i = 0;
