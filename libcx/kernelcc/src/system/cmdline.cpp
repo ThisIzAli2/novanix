ba38cfd5261ac3d9c42f2f30caecdc29cb2aaea1
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-14 18:05:11 -0400

    update cmdline.cpp

--- libcx/kernelcc/src/system/cmdline.cpp
+++ libcx/kernelcc/src/system/cmdline.cpp
@@ -184,7 +184,7 @@ VOID cmdline() {
                     ELIF(cmd_cmp(full_cmd,"wifi") == 0){
                         // scan_wifi_mac_addresses();
                     }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                     ELIF (cmd_cmp(full_cmd,"cowsay") == 0){
                         Novanix::system::printk(VGA_COLOR_WHITE,"\n",1);
                         Novanix::system::printk(VGA_COLOR_WHITE,"I love Nova!",1);
