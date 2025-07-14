bd94585722780bd8a4c7b7d80d690795237fb36d
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-14 18:06:02 -0400

    update cmdline.cpp

--- libcx/kernelcc/src/system/cmdline.cpp
+++ libcx/kernelcc/src/system/cmdline.cpp
@@ -146,6 +146,7 @@ VOID cmdline() {
                         Novanix::system::printk(Novanix::system::VGA_COLOR_RED,"TCP function not ready yet",1);
                         tcp_receive(19216811,19216811,(uint8_t*)3,19);
                     }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                     ELIF (cmd_cmp(full_cmd,"clear") == 0){
                         clear_screen();
                     }
