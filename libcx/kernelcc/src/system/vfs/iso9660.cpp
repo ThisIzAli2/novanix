2cf6d0731b0bfef2345c233d76268b77389e04f0
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-14 23:53:33 -0400

    Docs updated

--- libcx/kernelcc/src/system/vfs/iso9660.cpp
+++ libcx/kernelcc/src/system/vfs/iso9660.cpp
@@ -1,5 +1,23 @@
 //TODO: There is probably a bit of a memory leak here, fix this in the future
+/**
+ * @author Ali Mirmohammad
+ * @file iso9660.cpp
+ * @credits Remco123
+ ** This file is part of Novanix.
 
+**Novanix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**Novanix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with Novanix. If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <system/vfs/iso9660.h>
 
 #include <system/memory/heap.h>
