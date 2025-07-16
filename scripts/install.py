be926273391a2ace049e329700b642a3c3021370
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-16 18:19:09 -0400

    update install.py

--- scripts/install.py
+++ scripts/install.py
@@ -4,6 +4,12 @@ import subprocess
 
 
 def main():
+    subprocess.run(["cd",".."],check=True)
+    subprocess.run(["cd","libcx"],check=True)
     subprocess.run(["make", "clean"], check=False)
 
     subprocess.run(["make", "install"], check=True)
+
+
+if __name__ == "__main__":
+    main()
