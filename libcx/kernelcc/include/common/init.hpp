6b0f5f4f5a931e7f5cdf15911d164476491951a0
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-16 00:39:11 -0400

    update init.hpp

--- libcx/kernelcc/include/common/init.hpp
+++ libcx/kernelcc/include/common/init.hpp
@@ -34,10 +34,15 @@
 #include <novanix/access.h>
 #include <novanix/time/time.hpp>
 
-static inline void outl(uint16_t port, uint32_t val) {
+
+#ifndef __keep_inline 
+#define __keep_inline inline
+#endif //__keep_inline
+
+static __keep_inline void outl(uint16_t port, uint32_t val) {
     asm volatile("outl %0, %1" : : "a"(val), "Nd"(port));
 }
-static inline uint32_t inl(uint16_t port) {
+static __keep_inline uint32_t inl(uint16_t port) {
     uint32_t ret;
     asm volatile("inl %1, %0" : "=a"(ret) : "Nd"(port));
     return ret;
