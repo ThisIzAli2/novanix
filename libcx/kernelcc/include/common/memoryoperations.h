1eb662db2aef920bdc99ab43750af47d05b16936
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-16 16:38:05 -0400

    update memoryoperations.h

--- libcx/kernelcc/include/common/memoryoperations.h
+++ libcx/kernelcc/include/common/memoryoperations.h
@@ -32,10 +32,10 @@ namespace Novanix
         {
         public:
             // Memory operations 
-            static void* memmove(void* dstptr, const void* srcptr, uint32_t size);
-            static int memcmp(const void* aptr, const void* bptr, uint32_t size);
-            static void* memset(void* bufptr, char value, uint32_t size);
-            static void* memcpy(void* dstptr, const void* srcptr, uint32_t size);
+            static VOID* memmove(VOID* dstptr, const VOID* srcptr, uint32_t size);
+            static int memcmp(const VOID* aptr, const VOID* bptr, uint32_t size);
+            static VOID* memset(VOID* bufptr, char value, uint32_t size);
+            static VOID* memcpy(VOID* dstptr, const VOID* srcptr, uint32_t size);
         };
     }
 }
