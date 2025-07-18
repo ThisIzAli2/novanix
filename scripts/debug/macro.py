ea74dd1069b63100162eef1dd2ea9a43bf68ad41
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-18 18:55:13 -0400

    update macro.py

--- scripts/debug/macro.py
+++ scripts/debug/macro.py
@@ -1,2 +1,12 @@
 import os
 
+def contains(path):
+    try:
+        with open(path, 'r',encoding='utf-8',errors='ignore') as f:
+            for line in f:
+                if line.strip().startswith("#define"):
+                    return True
+                else:
+                    return False
+    except Exception as exc:
+        print(exc)
