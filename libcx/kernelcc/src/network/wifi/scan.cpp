d0854ebaa5bcaf2ca38e12aad32702b1650681bb
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-18 20:50:40 -0400

    create 1 file and delete 1 file

---
+++ libcx/kernelcc/src/network/wifi/scan.cpp
@@ -0,0 +1,23 @@
+/**
+ * @author Ali Mirmohammad
+ * @file scan.cpp
+ ** This file is part of Novanix.
+
+**Novanix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**Novanix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with Novanix. If not, see <https://www.gnu.org/licenses/>.
+*/
+#include <network/wifi/scan.h>
+
+unsigned int pci_read_config_dword(unsigned char bus, unsigned char device,unsigned char function, unsigned char offset){
+
+}
--- libcx/kernelcc/src/network/wifi/scanc.cpp
+++
@@ -1,5 +0,0 @@
-#include <network/wifi/scan.h>
-
-unsigned int pci_read_config_dword(unsigned char bus, unsigned char device,unsigned char function, unsigned char offset){
-    
-}
