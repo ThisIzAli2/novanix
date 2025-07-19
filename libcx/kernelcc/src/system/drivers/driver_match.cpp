965e976f77dc31c0e8eed7f4e9a6cac269df1f51
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-18 23:51:17 -0400

    update driver_match.cpp

--- libcx/kernelcc/src/system/drivers/driver_match.cpp
+++ libcx/kernelcc/src/system/drivers/driver_match.cpp
@@ -85,6 +85,6 @@ int match_driver(int found){
         case SEAGATE_VENDOR_ID:
             printk(VGA_WHITE,"		Seagate Technology driver found",1);
         case ADAPTEC_VENDOR_ID:
-            printk(VGA_WHITE,"			Adaptec Inc. (RAID/SCSI controllers) driver found",1);
+            printk(VGA_WHITE,"			Adaptec Inc. (RAID/SCSI controllers) driver found",1);            
     }
 }
