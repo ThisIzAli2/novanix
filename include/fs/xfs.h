311343a4ee439b7d26b190d119c51e6ee79f1d23
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-19 23:44:03 -0400

    update xfs.h

--- include/fs/xfs.h
+++ include/fs/xfs.h
@@ -1,3 +1,22 @@
+/**
+ * @author Ali Mirmohammad
+ * @file xfs.h
+ ** This file is part of Novanix.
+
+**Novanix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**Novanix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with Novanix. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #define BLOCK_SIZE 512  // 512 bytes per block
 #define NUM_BLOCKS 1024 // Number of blocks in the file system
 #define NUM_INODES 128  // Number of inodes
