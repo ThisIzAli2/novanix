7771bd2be588cc5a52a85098e2d8eeb62c6c313a
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-19 23:35:53 -0400

    update debug.py

--- scripts/debug/debug.py
+++ scripts/debug/debug.py
@@ -14,6 +14,7 @@ class Cmd:
         debug = parser.add_argument_group("Debugging")
 
         debug.add_argument("--macro-have",help="Check which files has macros.",required=False,action="store_true")
+        debug.add_argument("--doc-have",help="Check if the files have docs.",required=False,action="store_true")
         args = parser.parse_args()
         return args
 
@@ -21,6 +22,7 @@ class Cmd:
 def extract_command():
     args = Cmd.parse()
     macro_have = args.macro_have
+    doc_have = args.doc_have
 
     return ( 
         macro_have
@@ -37,10 +39,22 @@ def contains_macro(path):
     except Exception as exc:
         print(exc)
 
+def has_docs(path):
+    try:
+        with open(path, 'r',encoding='utf-8',errors='ignore') as f:
+            for line in f:
+                if line.strip().startswith("/**"):
+                    return True
+                else:
+                    return False
+    except Exception as exc:
+        print(exc)
+
+
 def main():
     args = Cmd.parse()
     macro_have = args.macro_have
-
+    doc_have = args.doc_have
     
     # Step 1: Change to parent directory
     os.chdir("..")
@@ -55,6 +69,13 @@ def main():
                     full_path = os.path.join(root, file)
                     if contains_macro(full_path):
                         print(f"#define found in: {full_path}")
+    elif doc_have:
+        for root, dirs, files in os.walk("."):
+            for file in files:
+                if file.endswith((".h", ".hpp", ".c", ".cpp")):
+                    full_path = os.path.join(root, file)
+                    if not has_docs(full_path):
+                        print(f"This file does not have docs defined in: {full_path}")
 
 
 if __name__ == "__main__":
