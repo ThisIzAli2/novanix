1aa3937a4c5d5d9b5e2833dd14e13a88d2a96f8a
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-21 18:11:34 -0400

    update fcreate.h

--- include/software/fcreate.h
+++ include/software/fcreate.h
@@ -30,7 +30,7 @@
 #define MAX_FILES 1000
 
 file_t __create_file(char* name,char* data,int size);
-void create_file_function(char* data);
+VOID create_file_function(char* data);
 file_t __create_empty_file(char* name);
 
 VOID create_empty_file(char* name);
