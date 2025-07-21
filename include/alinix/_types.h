1da600a8a6893bfada2598651844fdde905f83db
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-21 17:18:57 -0400

    update _types.h

--- include/alinix/_types.h
+++ include/alinix/_types.h
@@ -1,39 +1,39 @@
-/**
- * @author Ali Mirmohammad
- * @file _types.h
- ** This file is part of AliNix.
+// /**
+//  * @author Ali Mirmohammad
+//  * @file _types.h
+//  ** This file is part of AliNix.
 
-**AliNix is free software: you can redistribute it and/or modify
-**it under the terms of the GNU Affero General Public License as published by
-**the Free Software Foundation, either version 3 of the License, or
-**(at your option) any later version.
+// **AliNix is free software: you can redistribute it and/or modify
+// **it under the terms of the GNU Affero General Public License as published by
+// **the Free Software Foundation, either version 3 of the License, or
+// **(at your option) any later version.
 
-**AliNix is distributed in the hope that it will be useful,
-**but WITHOUT ANY WARRANTY; without even the implied warranty of
-**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-**GNU Affero General Public License for more details.
+// **AliNix is distributed in the hope that it will be useful,
+// **but WITHOUT ANY WARRANTY; without even the implied warranty of
+// **MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// **GNU Affero General Public License for more details.
 
-**You should have received a copy of the GNU Affero General Public License
-**along with AliNix. If not, see <https://www.gnu.org/licenses/>.
-*/
+// **You should have received a copy of the GNU Affero General Public License
+// **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
+// */
 
-/**
- * @abstract:
- *  - Kernel types.
-*/
+// /**
+//  * @abstract:
+//  *  - Kernel types.
+// */
 
-#ifndef ALINIX_KERNEL_DEMO_DEF_T_INCLUDED_H
-#define ALINIX_KERNEL_DEMO_DEF_T_INCLUDED_H
+// #ifndef ALINIX_KERNEL_DEMO_DEF_T_INCLUDED_H
+// #define ALINIX_KERNEL_DEMO_DEF_T_INCLUDED_H
 
-#include <stdio.h>
+// #include <stdio.h>
 
-#ifdef __cplusplus
-#define ALINIX_NULL_CPP_NULL_PTR (*(void ))
+// #ifdef __cplusplus
+// #define ALINIX_NULL_CPP_NULL_PTR (*(void ))
 
-#else 
+// #else 
 
 
-#endif  /* __cplusplus */
+// #endif  /* __cplusplus */
 
-#define ALINIX_NULL nullptr
-#endif /*__ALINIX_KERNEL_TYPES_H_HEADER_*/
+// #define ALINIX_NULL nullptr
+// #endif /*__ALINIX_KERNEL_TYPES_H_HEADER_*/
