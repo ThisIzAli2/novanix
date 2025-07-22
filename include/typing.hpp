678b8094f11c5022e3ce9f355c6d229b0e998000
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-22 00:12:31 -0400

    update typing.hpp

--- include/typing.hpp
+++ include/typing.hpp
@@ -24,9 +24,8 @@
 #define INTEGER int
 #define VOID void
 #define __always_inline inline
-#define __keep_inline inline
 #define ELIF else if
 #define BOOL bool
 #define LPVOID void*
 
-#endif
+#endif /*__TYPING_HPP_KERNEL*/
