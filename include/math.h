a0a6df4e293b8e8b1783fbe2195a6d81815d2b5d
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-23 19:20:47 -0400

    feat: create math.h

---
+++ include/math.h
@@ -0,0 +1,26 @@
+/**
+ * @author Ali Mirmohammad
+ * @file math.h
+ ** This file is part of Novanix.
+
+**Novanix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**Novanix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with Novanix. If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef __NOVANIX_KERNEL____MATH_H
+#define __NOVANIX_KERNEL____MATH_H
+
+#ifndef SQUARE
+#define SQUARE(x) ((x) * (x))
+#endif // SQUARE
+
+#endif /*__NOVANIX_KERNEL____MATH_H*/
