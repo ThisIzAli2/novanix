da56cf895ad13e2013146118414dcb2b8a30117e
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-23 19:24:24 -0400

    update gaming_generic.hpp

--- include/game/gaming_generic.hpp
+++ include/game/gaming_generic.hpp
@@ -25,7 +25,7 @@
 #define DASH printk(VGA_WHITE,"-",0);
 #define CLEAN printk(VGA_WHITE,"",1);
 #define AWAIT_CONSTANT 30
-#define PRINT_PIPE printk(VGA_WHITE,"|",0);
+#define PRINT_PIPE printk(VGA_WHITE,"|",1);
 
 class Snake{
     private:
