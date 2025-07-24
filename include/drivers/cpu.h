4aaa5152fcca846ec421af8b9c15d67cba90eb6c
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-24 18:54:34 -0400

    update cpu.h and cpu.cpp

--- include/drivers/cpu.h
+++ include/drivers/cpu.h
@@ -35,5 +35,13 @@ static __always_inline VOID cpuid(uint32_t reg, uint32_t *eax, uint32_t *ebx, ui
         : "0" (reg));
 }
 
+inline void __cpuid(uint32_t eax_in, uint32_t ecx_in, uint32_t* eax, uint32_t* ebx, uint32_t* ecx, uint32_t* edx) {
+    asm volatile ("cpuid"
+                  : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
+                  : "a" (eax_in), "c" (ecx_in));
+}
+
+VOID print_cpu_info();
+
 
 #endif // CPU_H
--- libcx/kernelcc/src/core/cpu.cpp
+++ libcx/kernelcc/src/core/cpu.cpp
@@ -69,3 +69,56 @@ VOID CPU::EnableFeatures()
         while(1);
     } 
 }
+
+VOID print_cpu_info(){
+    uint32_t eax, ebx, ecx, edx;
+
+    // Get CPU vendor
+    char vendor[13];
+    __cpuid(0, 0, &eax, &ebx, &ecx, &edx);
+    MemoryOperations::memcpy(&vendor[0], &ebx, 4);
+    MemoryOperations::memcpy(&vendor[4], &edx, 4);
+    MemoryOperations::memcpy(&vendor[8], &ecx, 4);
+    vendor[12] = '\0';
+    printk(VGA_WHITE,"CPU Vendor: %s\n",1,vendor);
+
+    // Get CPU features, family, model, stepping
+    __cpuid(1, 0, &eax, &ebx, &ecx, &edx);
+
+    uint8_t stepping = eax & 0xF;
+    uint8_t model = (eax >> 4) & 0xF;
+    uint8_t family = (eax >> 8) & 0xF;
+    uint8_t ext_model = (eax >> 16) & 0xF;
+    uint8_t ext_family = (eax >> 20) & 0xFF;
+
+    if (family == 6 || family == 15)
+        model += (ext_model << 4);
+    if (family == 15)
+        family += ext_family;
+
+    printk(VGA_WHITE,"CPU Family: %u\n", 1,family);
+    printk(VGA_WHITE,"CPU Model: %u\n", 1,family);
+    printk(VGA_WHITE,"CPU Stepping: %u\n", 1,stepping);
+
+    // Feature detection
+    if (edx & (1 << 23)) printk(VGA_WHITE,"MMX supported\n",1);
+    if (edx & (1 << 25)) printk(VGA_WHITE,"SSE supported\n",1);
+    if (edx & (1 << 26)) printk(VGA_WHITE,"SSE2 supported\n",1);
+    if (ecx & (1 << 0))  printk(VGA_WHITE,"SSE3 supported\n",1);
+    if (ecx & (1 << 9))  printk(VGA_WHITE,"SSSE3 supported\n",1);
+    if (ecx & (1 << 19)) printk(VGA_WHITE,"SSE4.1 supported\n",1);
+    if (ecx & (1 << 20)) printk(VGA_WHITE,"SSE4.2 supported\n",1);
+    if (ecx & (1 << 28)) printk(VGA_WHITE,"AVX supported\n",1);
+
+    // Full CPU brand string
+    char brand[49];
+    for (int i = 0; i < 3; i++) {
+        __cpuid(0x80000002 + i, 0, &eax, &ebx, &ecx, &edx);
+        MemoryOperations::memcpy(brand + i * 16, &eax, 4);
+        MemoryOperations::memcpy(brand + i * 16 + 4, &ebx, 4);
+        MemoryOperations::memcpy(brand + i * 16 + 8, &ecx, 4);
+        MemoryOperations::memcpy(brand + i * 16 + 12, &edx, 4);
+    }
+    brand[48] = '\0';
+    printk(VGA_WHITE,"CPU Brand: %s\n", 1,brand);
+}
