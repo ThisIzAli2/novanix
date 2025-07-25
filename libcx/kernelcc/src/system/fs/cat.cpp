b0cd3936f0331b3a5812ab05d18a1f89339c0fef
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-25 15:13:54 -0400

    update cat.cpp

--- libcx/kernelcc/src/system/fs/cat.cpp
+++ libcx/kernelcc/src/system/fs/cat.cpp
@@ -24,7 +24,7 @@
  */
 VOID cat_file(char* file) {
     for (INTEGER i = 0; i < MAX_FILES; i++) {
-        if (files[i].name == nullptr) continue; // skip empty entries, This is important
+        // if (files[i].name == nullptr) continue; // skip empty entries, This is important
         if (String::strcmp(files[i].name, file)) {
             printk(VGA_WHITE, files[i].data, 1);
             return;
