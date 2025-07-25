e65b917b53be8f9e7f02e3fc8dfa568d70c2e3a1
Heisenberg <alimirmohammad.1386@gmail.com>
2025-07-25 16:14:35 -0400

    update edit.cpp

--- libcx/kernelcc/src/software/edit.cpp
+++ libcx/kernelcc/src/software/edit.cpp
@@ -70,10 +70,10 @@ char* draw_editor() {
     clean:
     delete[] text;
     delete[] full_prompt; //? Keep it here?
-    // delete[] key_editor;//! Do not remove this
+    // delete[] key_editor;//! Do not remove this buffer
     exit:
     delete[] text;
-    // delete[] key_editor; //! Do not remove this
+    // delete[] key_editor; //! Do not remove this buffer
     delete[] full_prompt; //? Keep it here?
     return full_prompt;
 }
