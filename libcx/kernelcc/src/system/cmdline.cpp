1178e662881741f9d89c08fdbefef9e5bac476eb
Heisenberg <alimirmohammad.1386@gmail.com>
2025-08-01 19:36:49 -0400

    update cmdline.cpp

--- libcx/kernelcc/src/system/cmdline.cpp
+++ libcx/kernelcc/src/system/cmdline.cpp
@@ -496,6 +496,7 @@ VOID cmdline() {
                             
                         } else {
                             Novanix::system::printk(VGA_COLOR_GREEN,"Password saved successfully",1);
+                            have_access = false;
                         }
                         
                     }
