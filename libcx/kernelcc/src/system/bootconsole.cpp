1f140c77663c95e2b099e886f88eddcf666beac8
Heisenberg <alimirmohammad.1386@gmail.com>
2025-08-01 22:18:03 -0400

    update bootconsole.cpp

--- libcx/kernelcc/src/system/bootconsole.cpp
+++ libcx/kernelcc/src/system/bootconsole.cpp
@@ -1,5 +1,6 @@
 /**
  ** This file is part of AliNix.
+ @Credits Remco123
 
 **AliNix is free software: you can redistribute it and/or modify
 **it under the terms of the GNU Affero General Public License as published by
