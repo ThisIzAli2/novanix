df93602593f2be612b6b42ea62385459754904bb
Heisenberg <alimirmohammad.1386@gmail.com>
2025-08-10 21:37:53 -0400

    update fat32.h

--- include/fs/fat32.h
+++ include/fs/fat32.h
@@ -20,4 +20,57 @@
 #define __NOVANIX_KERNEL_FILE_SYSTEM__FAT_32__H
 
 
+#include <common/init.hpp>
+
+
+typedef struct __attribute__((packed)) {
+    uint8_t  jump_boot[3];
+    uint8_t  oem_name[8];
+    uint16_t bytes_per_sector;
+    uint8_t  sectors_per_cluster;
+    uint16_t reserved_sector_count;
+    uint8_t  num_fats;
+    uint16_t root_entry_count;    // usually 0 for FAT32
+    uint16_t total_sectors_16;
+    uint8_t  media;
+    uint16_t fat_size_16;         // FAT16 size, 0 for FAT32
+    uint16_t sectors_per_track;
+    uint16_t num_heads;
+    uint32_t hidden_sectors;
+    uint32_t total_sectors_32;
+
+    // FAT32 extended BPB
+    uint32_t fat_size_32;
+    uint16_t ext_flags;
+    uint16_t fs_version;
+    uint32_t root_cluster;
+    uint16_t fs_info;
+    uint16_t backup_boot_sector;
+    uint8_t  reserved[12];
+    uint8_t  drive_number;
+    uint8_t  reserved1;
+    uint8_t  boot_signature;
+    uint32_t volume_id;
+    uint8_t  volume_label[11];
+    uint8_t  fs_type[8];
+} fat32_bpb_t;
+
+// Directory entry structure (short name)
+typedef struct __attribute__((packed)) {
+    uint8_t  name[11];
+    uint8_t  attr;
+    uint8_t  nt_reserved;
+    uint8_t  creation_time_tenth;
+    uint16_t creation_time;
+    uint16_t creation_date;
+    uint16_t last_access_date;
+    uint16_t first_cluster_high;
+    uint16_t write_time;
+    uint16_t write_date;
+    uint16_t first_cluster_low;
+    uint32_t file_size;
+} fat32_dir_entry_t;
+
+
+
 #endif /*__NOVANIX_KERNEL_FILE_SYSTEM__FAT_32__H*/
